594b70b8451da2e3b3a1c3d45c514d49739f3088
KonradK2A <konrad.kihan@gmail.com>
2021-06-05 21:44:41 +0200

    Initial commit

---
+++ data_analisis.py
@@ -0,0 +1,11 @@
+"""
+responsible for analyzing and creating model based on how images look like, recognision of faces
+"""
+class OpticalAnalisis:
+    pass
+
+"""
+responsible for managing data and it's visualization also support for OpticalAnalisis
+"""
+class DataAnalisis:
+    pass
