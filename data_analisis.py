a2d144128fa8130cf0279c4ac45e114368c40378
KonradK2A <konrad.kihan@gmail.com>
2021-06-06 01:36:35 +0200

    Browse categories

--- data_analisis.py
+++ data_analisis.py
@@ -1,2 +1,16 @@
+from os.path import join, dirname, abspath
+from os import listdir
+
+
 class ImageRecgonition:
-    pass
+    def __init__(self, path: str):
+        self.path = path
+        
+
+    def find_categories(self) -> list:
+        # look for all subdirectories and name categories from them 
+        return [ d for d in listdir(join(dirname(abspath(__file__)), self.path)) ]
+
+
+if __name__ == "__main__":
+    ImageRecgonition("images").find_categories()
